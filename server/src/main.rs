6d4604d47c635ca2a79b7c0c4ed101be2dc1cd8c
Wafelack <wafelack@protonmail.com>
2020-10-30 16:59:40 +0100

    fix: being non utf 8 compatible

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,9 +12,9 @@ fn main() {
         let mut buf = [0; 1024];
         let (number_of_bytes, _src_addr) =
             socket.recv_from(&mut buf).expect("Failed to receive bytes");
-        let data_recv: &[u8] = &mut buf[..number_of_bytes];
 
-        let v: Value = serde_json::from_slice(data_recv).expect("Failed to parse header");
+        let v: Value =
+            serde_json::from_slice(&buf[..number_of_bytes]).expect("Failed to parse header");
 
         if let Value::String(s) = &v["name"] {
             if let Value::Number(n) = &v["size"] {
@@ -22,7 +22,7 @@ fn main() {
                 let name = s;
                 let mut bytes_received = 0usize;
 
-                let mut content = String::new();
+                let mut content: Vec<u8> = Vec::new();
 
                 while bytes_received < size as usize {
                     let mut tempbuf = [0; 4096];
@@ -31,13 +31,14 @@ fn main() {
                         .recv_from(&mut tempbuf)
                         .expect("Failed to receive bytes");
 
-                    content.push_str(
-                        std::str::from_utf8(&tempbuf[..b_amount]).expect("Failed to convert bytes"),
-                    );
+                    for e in &tempbuf[..b_amount] {
+                        content.push(*e);
+                    }
+
                     bytes_received += b_amount;
                 }
                 let mut f = File::create(name).expect("Failed to create file");
-                f.write_all(content.as_bytes())
+                f.write_all(content.as_slice())
                     .expect("Failed to write in file");
             } else {
                 continue;
